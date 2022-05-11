d452608d9a9fd4af4f6ae2d25c6680eb66d79253
Arnout Engelen <arnout@bzzt.net>
2022-05-11 10:57:21 +0200

    compile
    not yet using TLS though

--- Main.scala
+++ Main.scala
@@ -3,7 +3,7 @@ import akka.actor.typed.scaladsl._
 import akka.http._
 import akka.http.scaladsl._
 import akka.http.scaladsl.model.headers._
-import akka.http.scaladsl.server.directives.Directives._
+import akka.http.scaladsl.server.Directives._
 import org.slf4j.LoggerFactory
 
 @main
@@ -13,14 +13,11 @@ def main =
   log.debug("Starting up")
 
   given sys: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "test")
-  try
-    Http(sys)
-      .newServerAt("localhost", 8443)
-      .bind(
-        headerValueByType[`Tls-Session-Info`](`Tls-Session-Info`) { info =>
-          log.info(info)
-          complete()
-        }
-      )
-  finally
-    sys.terminate()
+  Http()
+    .newServerAt("localhost", 8443)
+    .bind(
+      headerValueByType[`Tls-Session-Info`](`Tls-Session-Info`) { info =>
+        log.info(info.toString)
+        complete("Thanks")
+      }
+    )
